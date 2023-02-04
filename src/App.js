0ca629c3bba58f125f6c006ec337f49e8cd79443
brainy-academy <124410115+brainy-academy@users.noreply.github.com>
2023-02-03 23:06:33 -0300

    [BRAIN-8] Create landing page code (#1)
    * chore: update readme
    
    * feat: added package.json and gitignore
    
    * feat: added public assets
    
    * feat: added source code
    
    ---------
    
    Co-authored-by: Natan Nascimento <natanascimentom@outlook.com>

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Em Construção
+        </h1>
+        <a
+          className="App-link"
+          href="mailto:customer@brainy.academy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fale Conosco
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
